123c2cdb79cf6f2fa95683f076c63238b1651936
Haseb Ansari <ansarihasebgmail.com>
2021-02-09 00:01:35 +0100

    spring-boot initial commit

---
+++ examples/VIDEO5/School/src/main/java/com/hi5code/example/SchoolApplication.java
@@ -0,0 +1,11 @@
+package com.hi5code.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApplication.class, args);
+    }
+}
---
+++ examples/VIDEO5/School/src/main/java/com/hi5code/example/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.hi5code.example.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
