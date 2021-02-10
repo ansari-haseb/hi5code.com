d7a0bc47c4d6a9c4aa72207ebf005aff62743afb
Haseb Ansari <ansarihasebgmail.com>
2021-02-10 14:33:21 +0100

    controllers with ftl page

--- examples/VIDEO5/School/src/main/java/com/hi5code/example/controllers/HelloController.java
+++ examples/VIDEO5/School/src/main/java/com/hi5code/example/controllers/HelloController.java
@@ -1,12 +1,12 @@
 package com.hi5code.example.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "hello";
     }
 }
