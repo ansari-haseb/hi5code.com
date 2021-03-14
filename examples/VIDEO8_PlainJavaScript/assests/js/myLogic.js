f9e3761fb68eed9b56a9e2df4bb683697c84bab2
Haseb Ansari <ansarihasebgmail.com>
2021-03-14 18:04:46 +0100

    JS application integrated with Keycloak

---
+++ examples/VIDEO8_PlainJavaScript/assests/js/myLogic.js
@@ -0,0 +1,31 @@
+var keycloak = new Keycloak();
+
+function initKeycloak() {
+    keycloak.init({onLoad: 'login-required'}).then(function() {
+        constructTableRows(keycloak.idTokenParsed);
+        pasteToken(keycloak.token);
+    }).catch(function() {
+        alert('failed to initialize');
+    });
+}
+
+function constructTableRows(keycloakToken) {
+    document.getElementById('row-username').innerHTML = keycloakToken.preferred_username;
+    document.getElementById('row-firstName').innerHTML = keycloakToken.given_name;
+    document.getElementById('row-lastName').innerHTML = keycloakToken.family_name;
+    document.getElementById('row-name').innerHTML = keycloakToken.name;
+    document.getElementById('row-email').innerHTML = keycloakToken.email;
+}
+
+function pasteToken(token){
+    document.getElementById('ta-token').value = token;
+    document.getElementById('ta-refreshToken').value = keycloak.refreshToken;
+}
+
+var refreshToken = function() {
+    keycloak.updateToken(10)
+    .then(function(){
+        document.getElementById('ta-token').value = keycloak.token;
+        document.getElementById('ta-refreshToken').value = keycloak.refreshToken;
+    });
+}
